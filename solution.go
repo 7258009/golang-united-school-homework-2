1518ecb6f8478def492a38e673eb03db94839f53
SergeyPro <95023756+7258009@users.noreply.github.com>
2022-06-22 00:21:47 +0300

    Update solution.go
    7

--- solution.go
+++ solution.go
@@ -22,7 +22,7 @@ func CalcSquare(sideLen float64, sidesNum YourTypeNameHere) float64 {
 
 	if (sidesNum == SidesSquare){
 		return ( ( sideLen * sideLen) / 16 )
-		( (sideLen/4) * (sideLen/4) )
+		//( (sideLen/4) * (sideLen/4) )
 	}
 
 	if (sidesNum == SidesTriangle){
